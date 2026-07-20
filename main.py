2cc4d2795a0e6a1ddad7b31000920b76f920c250
tarundb2005-create <tarundb2005@gmail.com>
2026-07-20 20:43:04 +0530

    Update and Delete

--- main.py
+++ main.py
@@ -30,3 +30,19 @@ def get_product_by_id(id:int):
 def add_product(product:Product):
     products.append(product)
     return product
+
+@app.put('/product')
+def update_product(id : int , product : Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "Product added successfully"
+    return "No product found"
+
+@app.delete('/product')
+def delete_product(id : int):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i]
+            return "Product is Successfully Deleted"
+    return "No Product Found"
