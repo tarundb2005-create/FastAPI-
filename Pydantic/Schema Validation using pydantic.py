47e0f78361735142aba488789cfbf5b49117ba7e
Tarun <tarundb2005@gmail.com>
2026-08-16 22:34:06 +0530

    Add FastAPI endpoints for creating posts
    Implement FastAPI with Pydantic for post creation.

--- Pydantic/Schema Validation using pydantic.py
+++ Pydantic/Schema Validation using pydantic.py
@@ -1,1 +1,32 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title : str
+    content : str
+
+#order is important same as first come first serve
+
+@app.get("/")
+def test():
+    return "Succesfully my api runs"
+
+@app.post("/createpost")
+def create_post(new_post : Post):
+    print(new_post.title)
+    return {
+       
+        "data": "new_post"
+    }
+
+
+#using postman api testing results :
+#{
+#    "title" : "doomsday",
+#    "content" : "avengers"
+#}
 
